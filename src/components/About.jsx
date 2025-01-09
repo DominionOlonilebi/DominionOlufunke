d2a4c2504e65b0f8f8aa0f7b81a8f91d2e850cdd
dominionolonilebi <dominionolufunke27@gmail.com>
2025-01-09 19:19:03 +0100

    updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -116,7 +116,7 @@ const About = () => {
           >
             <div className="card abt-card">
               <p data-aos="zoom-in" data-aos-duration="2500" className="px-3">
-                I'm a Fullstack Developer with 2years experience working on several projects, proficient in transforming
+                I'm a Fullstack Developer with 3 years experience working on several projects, proficient in transforming
                 conceptual designs into fully functional websites. with
                 expertise in <span style={{color: '#e7008a'}}>MERN</span>. Reactjs, JavaScript, Nextjs, Express, Node.js, MongoDB, Redux
                 Toolkits, Postgres, Context API, Bootstrap and Tailwind, i craft intuitive
